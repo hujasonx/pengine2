2dfbab161072d4ba9563515d83e07c4413a57c4e
Jason Hu <yo.imuh3@gmail.com>
2022-06-06 21:32:22 -0700

    use lwjgl3

--- desktop/src/com/phonygames/pengine/desktop/DesktopLauncher.java
+++ desktop/src/com/phonygames/pengine/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package com.phonygames.pengine.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.phonygames.pengine.PCore;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PCore(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.useOpenGL3(true, 3, 3);
+		config.setAudioConfig(64, 1024, 9);
+		config.setWindowedMode(1600, 900);
+		new Lwjgl3Application(new PCore(), config);
 	}
 }
