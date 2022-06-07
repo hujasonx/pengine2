9259391992ba3e28fe2c2c878b2dec031f075334
Jason Hu <yo.imuh3@gmail.com>
2022-06-06 20:31:14 -0700

    initial commit

---
+++ desktop/src/com/phonygames/pengine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.phonygames.pengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.phonygames.pengine.PCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PCore(), config);
+	}
+}
