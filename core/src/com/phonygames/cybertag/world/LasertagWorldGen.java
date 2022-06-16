cc8c6b6f37321b13cce36cf8fa18efea52825f39
Jason Hu <yo.imuh3@gmail.com>
2022-06-15 21:11:21 -0700

    refactor into lasertagworlgen

--- core/src/com/phonygames/cybertag/world/LasertagWorldGen.java
+++ core/src/com/phonygames/cybertag/world/LasertagWorldGen.java
@@ -1,8 +1,54 @@
 package com.phonygames.cybertag.world;
 
+import android.support.annotation.NonNull;
+
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.phonygames.pengine.graphics.material.PMaterial;
+import com.phonygames.pengine.graphics.model.PGlNode;
+import com.phonygames.pengine.graphics.model.PModel;
+import com.phonygames.pengine.graphics.model.PModelGen;
+import com.phonygames.pengine.graphics.model.PModelInstance;
+import com.phonygames.pengine.graphics.model.PVertexAttributes;
+import com.phonygames.pengine.math.PMat4;
+import com.phonygames.pengine.util.PList;
+
 public class LasertagWorldGen {
-
   public LasertagWorldGen() {
+  }
 
+  public void gen(@NonNull final OnFinishedCallback onFinishedCallback) {
+    PModelGen.getPostableTaskQueue().enqueue(new PModelGen() {
+      PModelGen.Part basePart;
+      PModelGen.StaticPhysicsPart basePhysicsPart;
+
+      @Override protected void modelIntro() {
+        basePart = addPart("base", new PVertexAttributes(
+            new VertexAttribute[]{PVertexAttributes.Attribute.get(PVertexAttributes.Attribute.Keys.pos),
+                                  PVertexAttributes.Attribute.get(PVertexAttributes.Attribute.Keys.nor)}));
+        basePhysicsPart = addStaticPhysicsPart("base");
+      }
+
+      @Override protected void modelMiddle() {
+        basePart.set(PVertexAttributes.Attribute.Keys.nor, 0, 0, 1);
+        basePart.set(PVertexAttributes.Attribute.Keys.pos, -1, -1, 0).emitVertex();
+        basePart.set(PVertexAttributes.Attribute.Keys.pos, 1, -1, 0).emitVertex();
+        basePart.set(PVertexAttributes.Attribute.Keys.pos, 1, 1, 0).emitVertex();
+        basePart.set(PVertexAttributes.Attribute.Keys.pos, -1, 1, 0).emitVertex();
+        basePart.quad(false, basePhysicsPart);
+      }
+
+      @Override protected void modelEnd() {
+        PList<PGlNode> glNodes = new PList<>();
+        chainGlNode(glNodes, basePart, new PMaterial(basePart.name(), null), null, "PBR");
+        PModel.Builder builder = new PModel.Builder();
+        emitStaticPhysicsPartIntoModelBuilder(builder);
+        builder.addNode("box", null, glNodes, PMat4.IDT);
+        onFinishedCallback.onFinished(builder.build());
+      }
+    });
+  }
+
+  static abstract class OnFinishedCallback {
+    public abstract void onFinished(PModel model);
   }
 }
--- core/src/com/phonygames/cybertag/world/World.java
+++ core/src/com/phonygames/cybertag/world/World.java
@@ -1,16 +1,9 @@
 package com.phonygames.cybertag.world;
 
-import com.badlogic.gdx.graphics.VertexAttribute;
 import com.phonygames.pengine.graphics.PRenderContext;
-import com.phonygames.pengine.graphics.material.PMaterial;
-import com.phonygames.pengine.graphics.model.PGlNode;
 import com.phonygames.pengine.graphics.model.PGltf;
 import com.phonygames.pengine.graphics.model.PModel;
-import com.phonygames.pengine.graphics.model.PModelGen;
 import com.phonygames.pengine.graphics.model.PModelInstance;
-import com.phonygames.pengine.graphics.model.PVertexAttributes;
-import com.phonygames.pengine.math.PMat4;
-import com.phonygames.pengine.util.PList;
 import com.phonygames.pengine.util.PStringMap;
 
 import lombok.AccessLevel;
@@ -25,44 +18,20 @@ public class World {
   @Accessors(fluent = true)
   private final PStringMap<PModel> models = new PStringMap<>();
   private PModelInstance testBoxModelInstance;
-  private PModel testBoxModel;
+  private PModel worldModel;
 
   public World() {
-    PModelGen.getPostableTaskQueue().enqueue(new PModelGen() {
-      PModelGen.Part basePart;
-      PModelGen.StaticPhysicsPart basePhysicsPart;
-
-      @Override protected void modelIntro() {
-        basePart = addPart("base", new PVertexAttributes(
-            new VertexAttribute[]{PVertexAttributes.Attribute.get(PVertexAttributes.Attribute.Keys.pos),
-                                  PVertexAttributes.Attribute.get(PVertexAttributes.Attribute.Keys.nor)}));
-        basePhysicsPart = addStaticPhysicsPart("base");
-      }
-
-      @Override protected void modelMiddle() {
-        basePart.set(PVertexAttributes.Attribute.Keys.nor, 1, 0, 0);
-        basePart.set(PVertexAttributes.Attribute.Keys.pos, -1, -1, 0).emitVertex();
-        basePart.set(PVertexAttributes.Attribute.Keys.pos, 1, -1, 0).emitVertex();
-        basePart.set(PVertexAttributes.Attribute.Keys.pos, 1, 1, 0).emitVertex();
-        basePart.set(PVertexAttributes.Attribute.Keys.pos, -1, 1, 0).emitVertex();
-        basePart.quad(false, basePhysicsPart);
-      }
-
-      @Override protected void modelEnd() {
-        PList<PGlNode> glNodes = new PList<>();
-        chainGlNode(glNodes, basePart, new PMaterial(basePart.name(), null), null, "PBR");
-        PModel.Builder builder = new PModel.Builder();
-        emitStaticPhysicsPartIntoModelBuilder(builder);
-        builder.addNode("box", null, glNodes, PMat4.IDT);
-        testBoxModel = builder.build();
-        testBoxModelInstance = new PModelInstance(testBoxModel);
+    LasertagWorldGen worldGen = new LasertagWorldGen();
+    worldGen.gen(new LasertagWorldGen.OnFinishedCallback() {
+      @Override public void onFinished(PModel model) {
+        worldModel = model;
+        testBoxModelInstance = new PModelInstance(worldModel);
         testBoxModelInstance.createAndAddStaticBodiesFromModelWithCurrentWorldTransform();
       }
     });
   }
 
   public void render(PRenderContext renderContext) {
-
     if (testBoxModelInstance != null) {
       testBoxModelInstance.enqueue(renderContext, PGltf.DEFAULT_SHADER_PROVIDER);
     }
