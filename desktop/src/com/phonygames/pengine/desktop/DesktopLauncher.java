c255cd2039d244cf850e5d7ffa1d599bdbae7fd3
Jason Hu <yo.imuh3@gmail.com>
2022-07-14 21:49:57 -0700

    improved reload animation

--- desktop/src/com/phonygames/pengine/desktop/DesktopLauncher.java
+++ desktop/src/com/phonygames/pengine/desktop/DesktopLauncher.java
@@ -2,19 +2,17 @@ package com.phonygames.pengine.desktop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Window;
 import com.phonygames.cybertag.CybertagGame;
 import com.phonygames.pengine.PEngine;
-import com.phonygames.pengine.PGame;
 
 public class DesktopLauncher {
-	private static Lwjgl3Application application;
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+  private static Lwjgl3Application application;
 
-		config.useOpenGL3(true, 3, 3);
-		config.setAudioConfig(64, 1024, 9);
-		config.setWindowedMode(1600, 900);
-		application = new Lwjgl3Application(new PEngine(new CybertagGame()), config);
-	}
+  public static void main(String[] arg) {
+    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    config.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL30, 3, 3);
+    config.setAudioConfig(64, 1024, 9);
+    config.setWindowedMode(1600, 900);
+    application = new Lwjgl3Application(new PEngine(new CybertagGame()), config);
+  }
 }
