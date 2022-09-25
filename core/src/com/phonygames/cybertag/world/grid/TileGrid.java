dd7b056421833da5b0951353a410a6547475b18d
Jason Hu <yo.imuh3@gmail.com>
2022-09-25 09:54:34 -0700

    add tilegrid offset from edge

--- core/src/com/phonygames/cybertag/world/grid/TileGrid.java
+++ core/src/com/phonygames/cybertag/world/grid/TileGrid.java
@@ -1,17 +1,24 @@
 package com.phonygames.cybertag.world.grid;
 
+import android.support.annotation.Nullable;
+
 import com.phonygames.pengine.exception.PAssert;
+import com.phonygames.pengine.math.aabb.PIntAABB;
+import com.phonygames.pengine.util.PFacing;
 import com.phonygames.pengine.util.collection.PIntMap3d;
 import com.phonygames.pengine.util.collection.PPooledIterable;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
 /**
- * Represents a world that uses a 3d grid representation.
+ * Represents a world that uses a 3d grid representation. Tiles should never be removed from the grid, only added.
  */
 public class TileGrid extends PPooledIterable<PIntMap3d.Entry<GridTile>> {
   /** The grid tiles managed by this grid. */
   private final PIntMap3d<GridTile> tiles = new PIntMap3d<GridTile>() {
-    @Override
-    protected GridTile newUnpooled(int x, int y, int z) {
+    @Override protected GridTile newUnpooled(int x, int y, int z) {
       GridTile.GridTileBuilder builder = new GridTile.GridTileBuilder();
       builder.x(x);
       builder.y(y);
@@ -19,10 +26,89 @@ public class TileGrid extends PPooledIterable<PIntMap3d.Entry<GridTile>> {
       return builder.build();
     }
   };
+  /** The bounds of this grid, or null if there are no tiles. */
+  @Getter(value = AccessLevel.PUBLIC)
+  @Accessors(fluent = true)
+  private @Nullable
+  PIntAABB bounds;
 
-  /** Returns the tile at the given coordinates. */
-  public GridTile getTileAt(int x, int y, int z) {
-    return tiles.get(x,y,z);
+  @Override public PPoolableIterator<PIntMap3d.Entry<GridTile>> obtainIterator() {
+    return tiles.obtainIterator();
+  }
+
+  /**
+   * Returns how many tiles away from the edge of this grid the tile is in the direction, where being on the edge will
+   * return 0.
+   *
+   * @param tile
+   * @param facing
+   * @return
+   */
+  public int offsetFromEdge(GridTile tile, PFacing facing) {
+    PAssert.isNotNull(bounds, "No bounds were set for this tile grid; it's not tracking anything!");
+    switch (facing) {
+      case X:
+        return bounds.x1() - tile.x;
+      case mX:
+        return tile.x - bounds.x0();
+      case Z:
+        return bounds.z1() - tile.z;
+      case mZ:
+        return tile.z - bounds.z0();
+      default:
+        PAssert.fail("Should not reach!");
+        return 0;
+    }
+  }
+
+  /**
+   * Returns how many tiles away from the top of this grid, where being on the top will
+   * return 0.
+   *
+   * @param tile
+   * @return
+   */
+  public int offsetFromTop(GridTile tile) {
+    PAssert.isNotNull(bounds, "No bounds were set for this tile grid; it's not tracking anything!");
+    return bounds.y1() - tile.y;
+  }
+
+  /**
+   * Returns how many tiles away from the bottom of this grid, where being on the bottom will
+   * return 0.
+   *
+   * @param tile
+   * @return
+   */
+  public int offsetFromBottom(GridTile tile) {
+    PAssert.isNotNull(bounds, "No bounds were set for this tile grid; it's not tracking anything!");
+    return tile.y - bounds.y0();
+  }
+
+  /** Tracks all tiles from the other tile grid. */
+  public void trackAll(TileGrid other) {
+    try (PPooledIterable.PPoolableIterator<PIntMap3d.Entry<GridTile>> it = other.tiles.obtainIterator()) {
+      while (it.hasNext()) {
+        PIntMap3d.Entry<GridTile> e = it.next();
+        trackTile(e.val());
+      }
+    }
+  }
+
+  /** Adds the given tile to the list of tiles managed by this grid. */
+  public void trackTile(GridTile tile) {
+    PAssert.isFalse(hasTileAt(tile.x, tile.y, tile.z));
+    if (bounds == null) {
+      bounds = PIntAABB.getStaticPool().obtain().set(tile.x, tile.y, tile.z, tile.x, tile.y, tile.z);
+    } else {
+      if (tile.x < bounds.x0()) {bounds.x0(tile.x);}
+      if (tile.y < bounds.y0()) {bounds.y0(tile.y);}
+      if (tile.z < bounds.z0()) {bounds.z0(tile.z);}
+      if (tile.x > bounds.x1()) {bounds.x1(tile.x);}
+      if (tile.y > bounds.y1()) {bounds.y1(tile.y);}
+      if (tile.z > bounds.z1()) {bounds.z1(tile.z);}
+    }
+    tiles.put(tile.x, tile.y, tile.z, tile);
   }
 
   /** Returns true if there is a tile at the coordinates. */
@@ -30,23 +116,8 @@ public class TileGrid extends PPooledIterable<PIntMap3d.Entry<GridTile>> {
     return getTileAt(x, y, z) != null;
   }
 
-  @Override public PPoolableIterator<PIntMap3d.Entry<GridTile>> obtainIterator() {
-    return tiles.obtainIterator();
-  }
-
-  /** Adds the given tile to the list of tiles managed by this grid. */
-  public void trackTile(GridTile tile) {
-    PAssert.isFalse(hasTileAt(tile.x,tile.y,tile.z));
-    tiles.put(tile.x, tile.y, tile.z, tile);
-  }
-
-  /** Tracks all tiles from the other tile grid. */
-  public void trackAll(TileGrid other) {
-    try (PPooledIterable.PPoolableIterator<PIntMap3d.Entry<GridTile>> it = other.tiles.obtainIterator()){
-      while (it.hasNext()) {
-        PIntMap3d.Entry<GridTile> e = it.next();
-        trackTile(e.val());
-      }
-    }
+  /** Returns the tile at the given coordinates. */
+  public GridTile getTileAt(int x, int y, int z) {
+    return tiles.get(x, y, z);
   }
 }
--- core/src/com/phonygames/pengine/math/aabb/PIntAABB.java
+++ core/src/com/phonygames/pengine/math/aabb/PIntAABB.java
@@ -1,6 +1,5 @@
 package com.phonygames.pengine.math.aabb;
 
-import com.phonygames.pengine.exception.PAssert;
 import com.phonygames.pengine.math.PRay;
 import com.phonygames.pengine.util.PPool;
 
@@ -10,28 +9,43 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 public class PIntAABB implements PPool.Poolable {
+  // #pragma mark - PPool.Poolable
   @Getter
   @Setter
   private PPool ownerPool, sourcePool;
-  @Getter(value = AccessLevel.PUBLIC)
-  @Accessors(fluent = true)
-  private int x0, y0, z0, x1, y1, z1;
-
+  // #pragma end - PPool.Poolable
   @Getter(value = AccessLevel.PUBLIC)
   private static final PPool<PIntAABB> staticPool = new PPool<PIntAABB>() {
     @Override protected PIntAABB newObject() {
       return new PIntAABB();
     }
   };
+  @Getter(value = AccessLevel.PUBLIC)
+  @Setter(value = AccessLevel.PUBLIC)
+  @Accessors(fluent = true)
+  private int x0, y0, z0, x1, y1, z1;
 
-  public boolean intersects(PIntAABB other) {
-    return (x0 <= other.x1 && x1 >= other.x0) && (y0 <= other.y1 && y1 >= other.y0) &&
-           (z0 <= other.z1 && z1 >= other.z0);
+  /**
+   * Returns true if this aabb contains the other. Aligned edges are considered contained.
+   */
+  public boolean fullyContains(PIntAABB other) {
+    if (!contains(other.x0, other.y0, other.z0)) {return false;}
+    if (!contains(other.x1, other.y1, other.z1)) {return false;}
+    return true;
+  }
+
+  public boolean contains(int x, int y, int z) {
+    return x >= x0 && x <= x1 && y >= y0 && y <= y1 && z >= z0 && z <= z1;
   }
 
   public boolean intersects(PRay ray) {
     float intersectLength = ray.intersectLength(this);
     return intersectLength >= 0;
+  }
+
+  public boolean intersects(PIntAABB other) {
+    return (x0 <= other.x1 && x1 >= other.x0) && (y0 <= other.y1 && y1 >= other.y0) &&
+           (z0 <= other.z1 && z1 >= other.z0);
   }
 
   @Override public void reset() {
@@ -43,15 +57,6 @@ public class PIntAABB implements PPool.Poolable {
     z1 = 0;
   }
 
-  /**
-   * Returns true if this aabb contains the other. Aligned edges are considered contained.
-   */
-  public boolean fullyContains(PIntAABB other) {
-    if (!contains(other.x0,other.y0, other.z0)) { return false; }
-    if (!contains(other.x1,other.y1, other.z1)) { return false; }
-    return true;
-  }
-
   public PIntAABB set(int x0, int y0, int z0, int x1, int y1, int z1) {
     this.x0 = Math.min(x0, x1);
     this.x1 = Math.max(x0, x1);
@@ -60,9 +65,5 @@ public class PIntAABB implements PPool.Poolable {
     this.z0 = Math.min(z0, z1);
     this.z1 = Math.max(z0, z1);
     return this;
-  }
-
-  public boolean contains(int x, int y, int z) {
-    return x >= x0 && x <= x1 && y >= y0 && y <= y1 && z >= z0 && z <= z1;
   }
 }
